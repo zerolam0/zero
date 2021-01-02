079b51b7dbf9f223816129da51c3b3d1c42d4cf6
zerolam0 <76870331+zerolam0@users.noreply.github.com>
2021-01-02 16:03:33 +0300

    Add files via upload

---
+++ x.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Legend" && password === "0987123") {
+        alert("You have successfully logged");
+        window.location.href = "k.html";
+        
+    } else {
+        alert("we have some error");
+    }
+})
